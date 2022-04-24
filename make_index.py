83cab369ba6c64f43457cb023e884390c168edef
iocpge <iocpge@gmail.com>
2022-04-24 21:06:46 +0200

    make ipt from dirs and files

--- make_index.py
+++ make_index.py
@@ -3,13 +3,12 @@ import dominate
 from dominate.tags import *
 import math
 
-
 doc = dominate.document(title='iocpge')
 
-
 with doc.head:
+    meta(name="title", content="Informatique en CPGE")
     meta(name="viewport", content="width=device-width, initial-scale=1")
-    #link(rel='stylesheet', href='style.css')
+    # link(rel='stylesheet', href='style.css')
     link(rel='stylesheet', href="https://latex.now.sh/style.css")
     link(rel="stylesheet", href="https://latex.now.sh/prism/prism.css")
     link(rel="stylesheet", href="https://latex.now.sh/lang/fr.css")
@@ -18,32 +17,26 @@ with doc.head:
     script(type='text/javascript', src="https://cdn.jsdelivr.net/npm/prismjs/prism.min.js")
     script("MathJax = {tex: { tags: 'ams'}};")
     script(id="MathJax-script", _async=True, src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js")
-    script(type='text/javascript', id="Local scripts",  src='script.js')
-    link(rel="icon",href="/favicon.ico")
-    link(rel="icon",href="/favicon.svg",type="image/svg+xml")
-    link(rel="apple-touch-icon",href="/apple-touch-icon.png")
+    script(type='text/javascript', id="Local scripts", src='script.js')
+    link(rel="icon", href="/favicon.ico")
+    link(rel="icon", href="/favicon.svg", type="image/svg+xml")
+    link(rel="apple-touch-icon", href="/apple-touch-icon.png")
     script(type='text/javascript', src='pseudocode.renderElement(document.getElementById("algorithm"));')
 
 with doc:
-    with div(id="Title"):
-        attr(cls="title")
-        p("Informatique en CPGE")
+    with div(id="header"):
+        h1("Informatique en CPGE")
 
-    with div(id="Author"):
-        attr(cls="author")
-        p("Io Cpge")
-        
     with div(id="abstract"):
         attr(cls="abstract")
-        h2("Résumé")     
-        p("Tout ce qu'il faut savoir et savoir faire pour réussir les épreuves d'informatique sereinement.")
-    with div(id='header').add(ol()):
-        for i in [['Informatique pour tous (IPT)', 'ipt'], ['Option informatique (MPSI/MP/MP*)', 'Optinfo']]:
-            li(a(i[0].title(), href='/%s.html' % i[1]))
+        h2("Résumé")
+        p("""Tout ce qu'il faut savoir et savoir faire pour réussir les épreuves d'informatique sereinement: 
+        cours, travaux pratiques sur machine, quizz, devoirs et corrigés. 
+        """)
 
-    with div():
-        attr(cls='body')
-        p("Let's start !")
- 
+    with div(id='topages').add(ol()):
+        for i in [['Informatique pour tous (IPT)', 'ipt'], ['Option informatique (MPSI/MP/MP*)', 'optinfo']]:
+            li(a(i[0], href='/%s.html' % i[1]))
+
+
 print(doc)
-
