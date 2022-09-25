db9675866b282856d4b1cc074d6eba903e92ae88
iocpge <iocpge@gmail.com>
2022-09-25 22:35:41 +0200

    mksite

--- ic/Semestre_1/2 - Programmation structurée/codes/square_function.py
+++
@@ -1,6 +0,0 @@
-def square(a):  # signature, a is a formal parameter
-    x = a * a  # function body
-    return x  # returned value
-
-nine = square(3)  # 3 is an effective parameter (argument)
-print("Squaring three gives ", nine)  # Squaring three gives 9.
--- ic/Semestre_1/2 - Programmation structurée/codes/square_script.py
+++
@@ -1,3 +0,0 @@
-x = 3
-sqrx = x * x
-print("Squaring 3 gives ", {sqrx})
