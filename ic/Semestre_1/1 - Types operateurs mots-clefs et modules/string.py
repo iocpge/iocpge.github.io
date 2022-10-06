687d0756ddd93d0e8732f5540242c08534663c8a
iocpge <iocpge@gmail.com>
2022-10-06 14:02:10 +0200

    corr cm

--- ic/Semestre_1/1 - Types operateurs mots-clefs et modules/string.py
+++ ic/Semestre_1/1 - Types operateurs mots-clefs et modules/string.py
@@ -12,7 +12,7 @@ from random import shuffle, sample
 
 deck = "78910VDRA"
 print(sample(deck, k=len(deck)))
-deck = [7,8,9,10,"V","D","R","A"]
+deck = ["7","8","9","10","V","D","R","A"]
 shuffle(deck)
 print(deck)
 
