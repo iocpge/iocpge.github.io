4b72d9e52bc0bc011ec1005942685ddb364235f3
iocpge <iocpge@gmail.com>
2022-12-11 12:15:18 +0100

    TP rec

--- ic/Semestre_1/5 - Récursivité/hanoi.py
+++ ic/Semestre_1/5 - Récursivité/hanoi.py
@@ -1,3 +1,7 @@
+import time
+
+global moves
+
 global start, aux, target
 start = []
 aux = []
@@ -18,6 +22,8 @@ def show_game():
     global start, aux, target
     size = max(max(start) if start else 0, max(aux) if aux else 0, max(target) if target else 0)
     print()
+    # pole = (" " * size + "|" + " " * size)
+    # print(f"     {pole * 3}")
     pole = (" " * size + " |" + " " * size)
     print("      ", pole * 3)
     for k in range(size, 0, -1):
@@ -25,28 +31,53 @@ def show_game():
         a = draw_stage(k, aux, size)
         t = draw_stage(k, target, size)
         print("#", k, " : ", s, a, t)
+        # print(f"#{k} : {s}{a}{t}")
 
 
 def init_game(n):
     global start, aux, target
-    # TODO :  INIT global start, aux and target
-    pass
+    start = [i for i in range(n, 0, -1)]
+    aux = []
+    target = []
 
 
 def move_from_a_to_b(from_a, to_b):
-    # TODO : show_game(), move disc from a to b, show_game()
-    pass
+    global moves  # mandatory because moves is modified below
+    show_game()
+    to_b.append(from_a.pop())
+    moves += 1
+    show_game()
 
 
 def hanoi(n, s, a, t):
-    # TODO : do not forget stop condition !
-    pass
+    if n == 1:
+        move_from_a_to_b(s, t)
+    else:
+        hanoi(n - 1, s, t, a)
+        move_from_a_to_b(s, t)
+        hanoi(n - 1, a, s, t)
 
 
 # MAIN PROGRAM
-n = 4
+moves = 0
+n = 5
 init_game(n)
 print(start, aux, target)
 show_game()
 hanoi(n, start, aux, target)
 show_game()
+
+print(moves)
+
+# for i in range(1, 23):
+#     moves = 0
+#     init_game(i)
+#     hanoi(i, start, aux, target)
+#     ui = 2 ** i - 1
+#     assert ui == moves
+
+# n = 30
+# init_game(n)
+# tic = time.process_time()
+# hanoi(n, start, aux, target)
+# print(time.process_time() - tic)  # 3 minutes environ sans affichage évidememnt
