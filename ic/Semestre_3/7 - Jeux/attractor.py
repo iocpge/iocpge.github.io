9bfa4e7bc760f37e02b740eb129c17358edb022d
iocpge <iocpge@gmail.com>
2023-02-01 18:43:47 +0100

    corr

--- ic/Semestre_3/7 - Jeux/attractor.py
+++ ic/Semestre_3/7 - Jeux/attractor.py
@@ -17,7 +17,7 @@ def attractor(g, V, Cg):
     n = len(g)
     A = {}  # attractor
     tg = g_transpose(g)  # graphe transposé
-    dsg = in_degrees(tg)  # degrés entrants de TG
+    dsg = out_degrees(g) # ou bien in_degrees(tg)  # degrés entrants de TG
     rank = 0
     for v in Cg:
         attract_grow_with(v, A, tg, dsg, V, rank)
@@ -25,47 +25,39 @@ def attractor(g, V, Cg):
 
 
 def out_degrees(g):
-    return [len(x) for x in g]
+    return {i:len(x) for i, x in enumerate(g)}
 
-
-# def in_degrees(g):
-#     flat_list = [i for adj in g for i in adj]
-#     in_deg = []
-#     c = Counter(flat_list)
-#     for v in range(len(g)):
-#         if v in c:
-#             in_deg.append(c[v])
-#         else:
-#             in_deg.append(0)
-#     return in_deg
-
-def in_degrees(g):
+def flatten(L):
     flat_list = []
     for adj in g:
         for i in adj:
             flat_list.append(i)
+    return flat_list
+
+def in_degrees(g):
     counts = {}
-    for i in flat_list:
-        counts[i] = counts.get(i, 0) + 1
-        # if i in counts:
-        #     counts[i] += 1
-        # else:
-        #     counts[i] = 1
+    for adj in g:
+        for i in adj:
+            # counts[i] = counts.get(i, 0) + 1
+            if i in counts:
+                counts[i] += 1
+            else:
+                counts[i] = 1
     return counts
+
 
 
 def g_transpose(g):
     tg = [[] for _ in range(len(g))]
     for i in range(len(g)):
-        for j in range(len(g)):
-            if i in g[j]:
-                tg[i].append(j)
+        for j in g[i]:
+            tg[j].append(i)
     return tg
 
 
 if __name__ == "__main__":
-    arene = [[], [6], [6,7], [6,7,8], [7, 8, 9], [8, 9, 10],
-             [], [0], [0,1], [0,1,2], [1, 2, 3], [2, 3, 4]]
+    arene = [[], [6], [6, 7], [6, 7, 8], [7, 8, 9], [8, 9, 10],
+             [], [0], [0, 1], [0, 1, 2], [1, 2, 3], [2, 3, 4]]
     V1 = {0, 1, 2, 3, 4, 5}  # PLAYER 1 VERTEX SET
     V2 = {6, 7, 8, 9, 10, 11}  # PLAYER 2 VERTEX SET
     Cg1 = {0: 0}
