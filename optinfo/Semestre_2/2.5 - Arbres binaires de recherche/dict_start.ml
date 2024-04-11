00508040b4464acc81bbc31bef20ddfa5c112d60
iocpge <iocpge@gmail.com>
2024-04-11 10:04:22 +0200

    autocorr

--- optinfo/Semestre_2/2.5 - Arbres binaires de recherche/dict_start.ml
+++ optinfo/Semestre_2/2.5 - Arbres binaires de recherche/dict_start.ml
@@ -32,7 +32,25 @@ let rec infix t = ();;
 
 let rec postfix t = ();;
 
-let avl_balance t = ();;
+let avl_balance t =
+   match t with
+   | Vide -> Vide
+   | Noeud(x,g,d) -> let (hg, hd) = (height g, height d) in
+               if hg > hd + 1 then
+               begin (* rotation à droite *)
+                    match g with
+                    | Noeud(y, a1, a2) when height a1 >= height a2 ->  Noeud(y, a1, Noeud(x,a2,d))
+                    | Noeud(y, a1, Noeud(z, a4, a5)) ->  Noeud(z, Noeud(y, a1, a4), Noeud(x,a5,d))
+                    | _ -> failwith "Unknown case"
+               end
+               else if hd > hg + 1 then
+                begin (* rotation à gauche *)
+                   match d with
+                    | Noeud(y, a1, a2) when height a2 >= height a1 ->  Noeud(y, Noeud(x,g,a1), a2)
+                    | Noeud(y, Noeud(z, a4, a5), a2) -> Noeud(z, Noeud(x, g, a4), Noeud(y,a5,a2))
+                    | _ -> failwith "Unknown case"
+                end
+               else t;;
 
 let rec badd d k v = ();;
 
